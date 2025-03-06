01f7b95fbe5e98c159ef43c5783b0764209387b8
AnthonyHeinz <anthonyheinz123@gmail.com>
2025-03-06 10:45:02 -0500

    Added testing

--- client/src/tests/BusinessDashboard.test.jsx
+++ client/src/tests/BusinessDashboard.test.jsx
@@ -5,26 +5,23 @@ import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 
 test('should render the BusinessDashboard component', () => {
-    render(
-        <MemoryRouter>
-            <BusinessDashboard />
-        </MemoryRouter>
-    );
+  render(
+    <MemoryRouter>
+      <BusinessDashboard />
+    </MemoryRouter>
+  );
 
-    // Check if the elements are rendered
-    expect(screen.getByText(/Welcome Business!/i)).toBeInTheDocument();
-    expect(screen.getByText(/Enroll a customer/i)).toBeInTheDocument();
+  expect(screen.getByText(/Welcome Business!/i)).toBeInTheDocument();
+  expect(screen.getByText(/Enroll a customer/i)).toBeInTheDocument();
 });
 
-// test('should navigate to the customer list when the button is clicked', () => {
-//     render(
-//         <MemoryRouter>
-//             <BusinessDashboard />
-//         </MemoryRouter>
-//     );
+test('should navigate to the customer list when the button is clicked', () => {
+  render(
+    <MemoryRouter>
+      <BusinessDashboard />
+    </MemoryRouter>
+  );
+  fireEvent.click(screen.getByText(/List Of Current Customers/i));
 
-//     // Assuming the customer list button exists and can be clicked
-//     fireEvent.click(screen.getByText(/List Of Current Customers/i));
-
-//     // Check if the URL changes (for simplicity, we expect a basic check)
-//     expect(window.location.pathname).toBe('/customerlist')})
+  expect(window.location.pathname).toBe('/customerlist');
+});
